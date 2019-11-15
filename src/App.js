de7a7bf50f2013677a0a2579c831ae3bfa1ca886
Billy Littlefield <billy.littlefield@airtable.com>
2019-11-15 08:48:18 -0800

    make the thing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
+import activity from './activity.json';
 import './App.css';
+
+let parsedData = activity.map(d => ({ ...d, time: new Date(d.time)}));
+parsedData = parsedData.filter(d => d.time > new Date('2019-01-01') && d.title.startsWith('Listened'));
+
+const byArtist = parsedData.reduce((acc, cur) => {
+  acc[cur.description] = acc[cur.description] ? acc[cur.description] + 1 : 1;
+  return acc;
+}, {});
+
+const bySong = parsedData.reduce((acc, cur) => {
+  acc[cur.title] = acc[cur.title] ? acc[cur.title] + 1 : 1;
+  return acc;
+}, {})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display: 'flex', margin: '40px', textAlign: 'left'}}>
+    <div>
+        <h2>Artists</h2>
+        {Object.keys(byArtist).sort((a, b) => byArtist[b] - byArtist[a]).map((artist, index) => {
+          return (
+            <div>{index + 1}. {artist}: {byArtist[artist]} listens</div>
+          )
+        })}
+      </div>
+      <div>
+        <h2>Songs</h2>
+        {Object.keys(bySong).sort((a, b) => bySong[b] - bySong[a]).map((song, index) => {
+          return (
+            <div>{index + 1}. {song.replace('Listened to ', '')}: {bySong[song]} listens</div>
+          )
+        })}
+      </div>
     </div>
   );
 }
